af1826965abd0c38db6c395ad973e411b1cc4601
DESKTOP-A6135OI\loko <harrypurmanta@gmail.com>
2026-07-20 23:13:47 +0700

    feat: serve compiled React frontend directly from CI4 root route index()

--- backend/app/Controllers/Home.php
+++ backend/app/Controllers/Home.php
@@ -4,8 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
+        $filePath = FCPATH . 'index.html';
+        if (file_exists($filePath)) {
+            return file_get_contents($filePath);
+        }
         return view('api_documentation');
     }
 
